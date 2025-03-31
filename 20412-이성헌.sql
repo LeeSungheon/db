575667eb9234f2117e15b7bc27395b40333d500f
LeeSungheon <qawsedrftg75833631@gmail.com>
2025-03-31 11:48:38 +0900

    Add files via upload

---
+++ 20412-이성헌.sql
@@ -0,0 +1,12 @@
+/*데이터구조*/
+desc emp;
+select*from emp;
+select ename job from emp;
+_데이터구조
+desc professor;
+select*from professor;
+select name, 'good morning~~!' "인사" from professor;
+select dname, 'lt''s deptno:',deptno "DNAME AND DEPTNO"FROM dept;
+select dname, 'lt''s deptno:'||deptno "DNAME AND DEPTNO"FROM dept;
+seleCt dname, q'l,it's deptno:l',deptno"DNAME AND DEPTNO"FROM dept;
+seleCt profno,"교수님 번호" name 이름 ,pay as"급여" from professor;
